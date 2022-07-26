b4a08fe99974dac736f5e112c54001172b797b14
Marijus Petraitis <marijus.petraitis@realcore.de>
2022-07-26 13:47:23 +0200

    Add project files.

---
+++ ShopOnline.Api/Data/ShopOnlineDbContext.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Data
+{
+    public class ShopOnlineDbContext:DbContext
+    {
+        public ShopOnlineDbContext(DbContextOptions<ShopOnlineDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+			//Products
+			//Electronics Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 1,
+				Name = "Air Pods",
+				Description = "Air Pods - in-ear wireless headphones",
+				ImageURL = "/Images/Electronic/Electronics1.png",
+				Price = 100,
+				Qty = 120,
+				CategoryId = 2
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 2,
+				Name = "On-ear Golden Headphones",
+				Description = "On-ear Golden Headphones - these headphones are not wireless",
+				ImageURL = "/Images/Electronic/Electronics2.png",
+				Price = 40,
+				Qty = 200,
+				CategoryId = 2
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 3,
+				Name = "On-ear Black Headphones",
+				Description = "On-ear Black Headphones - these headphones are not wireless",
+				ImageURL = "/Images/Electronic/Electronics3.png",
+				Price = 40,
+				Qty = 300,
+				CategoryId = 2
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 4,
+				Name = "Sennheiser Digital Camera with Tripod",
+				Description = "Sennheiser Digital Camera - High quality digital camera provided by Sennheiser - includes tripod",
+				ImageURL = "/Images/Electronic/Electronic4.png",
+				Price = 600,
+				Qty = 20,
+				CategoryId = 2
+
+			});
+
+			//Furniture Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 5,
+				Name = "Black Leather Office Chair",
+				Description = "Very comfortable black leather office chair",
+				ImageURL = "/Images/Furniture/Furniture1.png",
+				Price = 50,
+				Qty = 212,
+				CategoryId = 1
+			});
+
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 6,
+				Name = "Pink Leather Office Chair",
+				Description = "Very comfortable pink leather office chair",
+				ImageURL = "/Images/Furniture/Furniture2.png",
+				Price = 50,
+				Qty = 112,
+				CategoryId = 1
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 7,
+				Name = "Lounge Chair",
+				Description = "Very comfortable lounge chair",
+				ImageURL = "/Images/Furniture/Furniture3.png",
+				Price = 70,
+				Qty = 90,
+				CategoryId = 1
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 8,
+				Name = "Silver Lounge Chair",
+				Description = "Very comfortable Silver lounge chair",
+				ImageURL = "/Images/Furniture/Furniture4.png",
+				Price = 120,
+				Qty = 95,
+				CategoryId = 1
+			});
+
+			
+
+			//Add users
+			modelBuilder.Entity<User>().HasData(new User
+			{
+				Id = 1,
+				UserName = "Marijus"
+
+			});
+			modelBuilder.Entity<User>().HasData(new User
+			{
+				Id = 2,
+				UserName = "Sarah"
+
+			});
+
+			//Create Shopping Cart for Users
+			modelBuilder.Entity<Cart>().HasData(new Cart
+			{
+				Id = 1,
+				UserId = 1
+
+			});
+			modelBuilder.Entity<Cart>().HasData(new Cart
+			{
+				Id = 2,
+				UserId = 2
+
+			});
+			//Add Product Categories
+			
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 1,
+				Name = "Furniture",
+				IconCSS ="fas fa-couch"
+			});
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 2,
+				Name = "Electronics",
+				IconCSS = "fas fa-headphones"
+			});
+
+
+		}
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ ShopOnline.Api/Migrations/20220726102804_setdatabase.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopOnline.Api.Migrations
+{
+    public partial class setdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IconCSS = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ProductCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Carts",
+                columns: new[] { "Id", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "IconCSS", "Name" },
+                values: new object[,]
+                {
+                    { 1, "fas fa-couch", "Furniture" },
+                    { 2, "fas fa-headphones", "Electronics" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "Marijus" },
+                    { 2, "Sarah" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Qty" },
+                values: new object[,]
+                {
+                    { 1, 2, "Air Pods - in-ear wireless headphones", "/Images/Electronic/Electronics1.png", "Air Pods", 100m, 120 },
+                    { 2, 2, "On-ear Golden Headphones - these headphones are not wireless", "/Images/Electronic/Electronics2.png", "On-ear Golden Headphones", 40m, 200 },
+                    { 3, 2, "On-ear Black Headphones - these headphones are not wireless", "/Images/Electronic/Electronics3.png", "On-ear Black Headphones", 40m, 300 },
+                    { 4, 2, "Sennheiser Digital Camera - High quality digital camera provided by Sennheiser - includes tripod", "/Images/Electronic/Electronic4.png", "Sennheiser Digital Camera with Tripod", 600m, 20 },
+                    { 5, 1, "Very comfortable black leather office chair", "/Images/Furniture/Furniture1.png", "Black Leather Office Chair", 50m, 212 },
+                    { 6, 1, "Very comfortable pink leather office chair", "/Images/Furniture/Furniture2.png", "Pink Leather Office Chair", 50m, 112 },
+                    { 7, 1, "Very comfortable lounge chair", "/Images/Furniture/Furniture3.png", "Lounge Chair", 70m, 90 },
+                    { 8, 1, "Very comfortable Silver lounge chair", "/Images/Furniture/Furniture4.png", "Silver Lounge Chair", 120m, 95 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+        }
+    }
+}
